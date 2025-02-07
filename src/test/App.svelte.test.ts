1088bc160b4d8e6906cfa2b97c922ae715b7d95a
Komi Alasse <komialasse@gmail.com>
2025-02-06 18:10:38 -0800

    setup vitest

--- src/test/App.svelte.test.ts
+++ src/test/App.svelte.test.ts
@@ -1,7 +1,7 @@
 import { test, expect } from 'vitest'
 import { render } from '@testing-library/svelte';
 
-import View from '../src/lib/View.svelte'
+import View from '../lib/View.svelte'
 
 test('App', () => {
     expect(render(View)).toBeDefined()
